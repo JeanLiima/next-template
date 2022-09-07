7a6e7012279939175ad8390c3938c5d3db20910b
JeanLiima <jean.car.liima@gmail.com>
2022-09-07 12:44:12 -0300

    Add the configurations

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
+const webpack = require("webpack");
+const path = require('path')
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    sassOptions: {
+      includePaths: [path.join(__dirname, 'styles')],
+    },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      config.plugins.push(
+        new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery",
+        })
+      );
+    return config;
+    },
+ }
 
 module.exports = nextConfig
