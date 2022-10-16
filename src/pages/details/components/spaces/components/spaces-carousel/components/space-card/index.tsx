00b7df994fe97fe77ddfef95776391e6ac9e6565
JeanLiima <jean.car.liima@gmail.com>
2022-10-16 09:36:27 -0300

    Create details page

---
+++ src/pages/details/components/spaces/components/spaces-carousel/components/space-card/index.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image'
+import Link from 'next/link';
+
+import { HiOutlineUserGroup, HiOutlineArrowNarrowRight } from 'react-icons/hi'
+
+import styles from './styles.module.scss'
+
+interface ISpaceCard {
+    id: string,
+    name: string,
+    // img: {
+    //     src: string,
+    //     width: number,
+    //     height: number,
+    // },
+    // alt: string,
+    description: string,
+    quantity: number,
+}
+
+const SpaceCard = ({id, name, description, quantity}: ISpaceCard) => (
+    <div className={styles['place-card']}>
+        <div className={styles['place-card__image']}>
+            {/* <Image src={img.src} width={img.width} height={img.height} alt={alt}/> */}
+        </div>
+        <div className={styles['place-card__description']}>
+            <h2>{name}</h2>
+            <p>
+                {description}
+            </p>
+            <div className={styles['place-card__description__footer']}>
+                <div className={styles['place-card__description__footer__quantity']}>
+                    <div className={styles['place-card__description__footer__quantity__icon']}>
+                        <HiOutlineUserGroup/> 
+                    </div>
+                    {quantity}
+                </div>
+                <a href={`/details/${id}`}>
+                    reservar
+                    <HiOutlineArrowNarrowRight/>
+                </a>
+            </div>
+        </div>
+    </div>
+);
+
+export { SpaceCard }
---
+++ src/pages/details/components/spaces/components/spaces-carousel/index.tsx
@@ -0,0 +1,42 @@
+import { Carousel } from 'components/carousel'
+
+import { SpaceCard } from './components/space-card'
+
+interface ISpace {
+    uuid: string,
+    name: string,
+    description: string,
+};
+
+interface ISpaces {
+    list: Array<ISpace>
+}
+    
+const SpacesCarousel = ({ list }: ISpaces) => (
+    <Carousel
+        dots={false}
+        nav={false}
+        autoplay={false}
+        autoplayTimeout={2000}
+        autoplaySpeed={2000}
+        autoplayHoverPause={false}
+        margin={60}
+        autoWidth
+    >
+        {list?.map(space => {
+            console.log(space)
+            return (
+            <SpaceCard 
+                key={space.uuid}
+                id={space.uuid}
+                name={space.name}
+                // img={space.img}
+                // alt={space.alt}
+                description={space.description}
+                quantity={20}
+            />
+        )})}
+    </Carousel>
+)
+
+export { SpacesCarousel }
